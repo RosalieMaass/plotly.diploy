75abdc2529294a85442dd5be115997427bfa977d
Rosalie Adele <rosalie.maass@gmail.com>
2021-10-03 15:10:42 -0500

    update

--- plots.js
+++ plots.js
@@ -1,57 +1,37 @@
-function init() {
-  var selector = d3.select("#selDataset");
-  
-  d3.json("samples.json").then((data) => {
-    console.log(data);
-    var sampleNames = data.names;
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
-    });
-})}
+// Sort the data array using the greekSearchResults value
+data.sort(function(a, b) {
+  return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
+});
 
-init();
-function optionChanged(newSample) {
-  buildMetadata(newSample);
-  buildCharts(newSample);
-}
-function buildMetadata(sample) {
-  d3.json("samples.json").then((data) => {
-    var metadata = data.metadata;
-    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-    var result = resultArray[0];
-    var PANEL = d3.select("#sample-metadata");
+// Slice the first 10 objects for plotting
+data = data.slice(0, 10);
 
-    PANEL.html("");
-    console.log(result);
-    PANEL.append("h6").text(result.location);
-    PANEL.append("h6").text(result.id);
-    PANEL.append("h6").text(result.age);
-    PANEL.append("h6").text(result.gender);
-    PANEL.append("h6").text(result.ethnicity);
-    PANEL.append("h6").text(result.wfreq);
-    PANEL.append("h6").text(result.bbtype);
-  });
-}
+// Reverse the array due to Plotly's defaults
+data = data.reverse();
 
-// const demographics = {
-//    age,
-//    bbtype,
-//    ethnicity,
-//    gender,
-//    id,
-//    location,
-//    wfreq,
-//  }
- 
-//  const entries = Object.entries(demographics)
-//  console.log(entries)
+// Trace1 for the Greek Data
+var trace1 = {
+  x: data.map(row => row.greekSearchResults),
+  y: data.map(row => row.greekName),
+  text: data.map(row => row.greekName),
+  name: "Greek",
+  type: "bar",
+  orientation: "h"
+};
 
-//  Object.entries(demographics).forEach([key,val]) 
-//   => {
-//       console.log(val)
-//   }
-//   console.log(newSample);
-// }
+// data
+var data = [trace1];
+
+// Apply the group bar mode to the layout
+var layout = {
+  title: "Greek gods search results",
+  margin: {
+    l: 100,
+    r: 100,
+    t: 100,
+    b: 100
+  }
+};
+
+// Render the plot to the div tag with id "plot"
+Plotly.newPlot("plot", data, layout);
